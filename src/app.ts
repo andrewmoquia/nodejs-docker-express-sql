c8fd8eda50518cd5f6dcdff0548f6fc35dd9fae7
andrewmoquia <andrewmoquia09@gmail.com>
2022-03-11 13:11:43 +0800

    Change class type to static.

--- src/app.ts
+++ src/app.ts
@@ -8,26 +8,19 @@ import { ErrorHandling } from './middleware/errorHandling';
 import Routers from './routes';
 
 export class App {
-   private app: Express;
-   private port: string | undefined;
-   private origin: string | undefined;
-   private cookieSecret: string | undefined;
+   private static app = express();
+   private static port = process.env.PORT;
+   private static origin = process.env.ORIGIN;
+   private static cookieSecret = process.env.COOKIE_SECRET;
 
-   constructor() {
-      this.app = express();
-      this.port = process.env.PORT;
-      this.origin = process.env.ORIGIN;
-      this.cookieSecret = process.env.COOKIE_SECRET;
-   }
-
-   public start() {
+   public static start() {
       //Allow test in localhost:3000.
       this.app.set('trust proxy', 1);
 
       //Necessary to get access to ip for rate limiting.
       this.app.enable('trust proxy');
 
-      //Connect to the database
+      //Connect to the database.
       Database.connect();
 
       //Create redis server session
@@ -41,30 +34,30 @@ export class App {
          })
       );
 
-      //Recognize the incoming Request Object as a JSON Object
+      //Recognize the incoming Request Object as a JSON Object.
       this.app.use(express.json());
 
-      //Parse application/x-www-form-urlencoded, basically can only parse incoming Request Object if strings or arrays
+      //Parse application/x-www-form-urlencoded, basically can only parse incoming Request Object if strings or arrays.
       this.app.use(express.urlencoded({ extended: false }));
 
       //Parse Cookie header and populate req.cookies with an object keyed by the cookie names.
       this.app.use(cookieParser(this.cookieSecret));
 
-      //Add 11 layer of security
+      //Add 11 layer of security.
       //https://helmetjs.github.io/
       this.app.use(helmet());
 
-      //Handle routes
+      //Handle routes.
       this.app.use('/app/v1', Routers);
       this.app.get('/app/vi', (req, res) => {
          res.send('<h1>Hello World!!!!</h1>');
          console.log('Running');
       });
 
-      //Handle catching error responds
+      //Handle catching error responds.
       this.app.use(ErrorHandling.catch());
 
-      //Initiate the server app
+      //Initiate the server app.
       this.app.listen(this.port, () => {
          console.log(`Server is up at http://localhost:${this.port}`);
       });
