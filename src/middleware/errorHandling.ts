86b832c17a2a5e1dd3d7828d4a4ebef302877c58
andrewmoquia <andrewmoquia09@gmail.com>
2022-03-12 23:22:48 +0800

    Removed unused vars.

--- src/middleware/errorHandling.ts
+++ src/middleware/errorHandling.ts
@@ -1,10 +1,10 @@
-import { Request, Response, NextFunction, RequestHandler } from 'express';
+import { Request, Response } from 'express';
 
 export type ResponseError = Error & { status: number };
 
 export class ErrorHandling {
    public static catch() {
-      return (error: ResponseError, req: Request, res: Response, next: NextFunction) => {
+      return (error: ResponseError, req: Request, res: Response) => {
          res.status(error.status || 500);
          res.json({
             error: {
